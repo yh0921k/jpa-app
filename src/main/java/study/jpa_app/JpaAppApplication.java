9c576643584d6eff4617c88e24bd21307be76b93
Yonghwi Kim <yh0921k@gmail.com>
2022-11-03 10:00:00 +0900

    init: JPA 학습을 위한 저장소 초기화
    README.md
    - 저장소의 기반이 되는 강의 내용 추가
    
    .gitignore
    - Git 추적 제외 파일 추가
    
    buile.gradle
    - 자바 버전 변경(17 -> 11)
    - H2 Database 의존성 버전 변경

---
+++ src/main/java/study/jpa_app/JpaAppApplication.java
@@ -0,0 +1,13 @@
+package study.jpa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAppApplication.class, args);
+	}
+
+}
