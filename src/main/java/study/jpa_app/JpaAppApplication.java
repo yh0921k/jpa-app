f9b6f99c6d53d1b579a4bf1b944c8289b7eb47a7
Yonghwi Kim <yh0921k@gmail.com>
2022-11-07 11:35:12 +0900

    add: Section 01. View 환경설정

--- src/main/java/study/jpa_app/JpaAppApplication.java
+++ src/main/java/study/jpa_app/JpaAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaAppApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(JpaAppApplication.class, args);
+  }
 }
