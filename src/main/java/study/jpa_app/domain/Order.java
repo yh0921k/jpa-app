1d8883604d51635b2464725d2312bdfa0ab9161d
Yonghwi Kim <yh0921k@gmail.com>
2022-11-08 18:24:57 +0900

    add: Section 02. 엔티티 설계시 주의점(연관관계 편의 메서드)

--- src/main/java/study/jpa_app/domain/Order.java
+++ src/main/java/study/jpa_app/domain/Order.java
@@ -35,4 +35,19 @@ public class Order {
 
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
+
+  public void setMember(Member member) {
+    this.member = member;
+    member.getOrders().add(this);
+  }
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem);
+    orderItem.setOrder(this);
+  }
+
+  public void setDelivery(Delivery delivery) {
+    this.delivery = delivery;
+    delivery.setOrder(this);
+  }
 }
