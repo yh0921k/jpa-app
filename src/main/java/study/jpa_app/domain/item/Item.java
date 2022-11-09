02017e40236bd82122a0807e4a4264f66c7f4365
Yonghwi Kim <yh0921k@gmail.com>
2022-11-09 12:09:21 +0900

    add: Section 05. Item 비즈니스 로직 추가

--- src/main/java/study/jpa_app/domain/item/Item.java
+++ src/main/java/study/jpa_app/domain/item/Item.java
@@ -3,6 +3,7 @@ package study.jpa_app.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import study.jpa_app.domain.Category;
+import study.jpa_app.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -28,4 +29,18 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if(restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+
+    this.stockQuantity = restStock;
+  }
+
 }
