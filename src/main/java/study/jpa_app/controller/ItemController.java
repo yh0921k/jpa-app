9f71d301492ddc9470c133af398e9d9dad1837c7
Yonghwi Kim <yh0921k@gmail.com>
2022-11-10 12:32:56 +0900

    add: Section 07. 상품 등록

---
+++ src/main/java/study/jpa_app/controller/ItemController.java
@@ -0,0 +1,37 @@
+package study.jpa_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.jpa_app.domain.item.Book;
+import study.jpa_app.service.ItemService;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+  private final ItemService itemService;
+
+  @GetMapping("/items/new")
+  public String createForm(Model model) {
+    model.addAttribute("form", new BookForm());
+    return "items/createItemForm";
+  }
+
+  @PostMapping("/items/new")
+  public String create(BookForm bookForm) {
+
+    // Setter를 제거하고 create 등의 메서드를 만드는 것이 더 좋은 설계
+    Book book = new Book();
+    book.setName(bookForm.getName());
+    book.setPrice(bookForm.getPrice());
+    book.setStockQuantity(bookForm.getStockQuantity());
+    book.setAuthor(bookForm.getAuthor());
+    book.setIsbn(bookForm.getIsbn());
+
+    itemService.saveItem(book);
+    return "redirect:/";
+  }
+}
