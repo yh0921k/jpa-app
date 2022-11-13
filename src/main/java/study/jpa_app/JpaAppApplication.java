3616fd2a34d48fa320f7b43981ceadd8b98ff4d8
Yonghwi Kim <yh0921k@gmail.com>
2022-11-13 12:44:45 +0900

    add: Section 03. 간단한 주문 조회 V1(엔티티를 직접 노출)

--- src/main/java/study/jpa_app/JpaAppApplication.java
+++ src/main/java/study/jpa_app/JpaAppApplication.java
@@ -1,12 +1,21 @@
 package study.jpa_app;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaAppApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JpaAppApplication.class, args);
+  }
+
+  @Bean
+  Hibernate5Module hibernate5Module() {
+    Hibernate5Module hibernate5Module = new Hibernate5Module();
+//    hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+    return hibernate5Module;
   }
 }
