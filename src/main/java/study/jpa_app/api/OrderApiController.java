c25bad0fecf8ec2cdeaa62f1f61a208239df09de
Yonghwi Kim <yh0921k@gmail.com>
2022-11-14 23:53:32 +0900

    add: Section 04. 주문 조회 V1(엔티티 직접 노출)

---
+++ src/main/java/study/jpa_app/api/OrderApiController.java
@@ -0,0 +1,31 @@
+package study.jpa_app.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.jpa_app.domain.Order;
+import study.jpa_app.domain.OrderItem;
+import study.jpa_app.repository.OrderRepository;
+import study.jpa_app.repository.OrderSearch;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+  private final OrderRepository orderRepository;
+
+  @GetMapping("/api/v1/orders")
+  public List<Order> ordersV1() {
+    List<Order> all = orderRepository.findAll(new OrderSearch());
+    for (Order order : all) {
+      order.getMember().getName();
+      order.getDelivery().getAddress();
+
+      List<OrderItem> orderItems = order.getOrderItems();
+      orderItems.stream().forEach(o -> o.getItem().getName());
+    }
+    return all;
+  }
+}
